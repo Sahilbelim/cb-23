2cf1e520c3caf837b0c59c31e3389748a4924cad
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-09-11 16:59:17 +0530

    function

---
+++ function1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void printline()
+{
+    printf("\n-------------------------------------------\n");
+}
+void printplush()
+{
+    printf("\n+++++++++++++++++++++++++++++++++\n");
+    
+}
+void main()
+{
+    printf("Name : Belim sahil \n");
+
+    printline();
+
+    printf("Age  : 18\n");
+    
+    printline();
+   
+    printf("Weight : 45.36 \n");
+
+    printline();
+    printplush();
+}
---
+++ function2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void printsymbol(int n,char s)
+{
+    int i;
+    printf("\n");
+    for (i = 1; i <= n; i++)
+    {
+        printf("%c", s);
+    }
+    printf("\n");
+}
+void main()
+{
+    int num;
+    char symbol;
+
+    printf("Enter Symbol ");
+    scanf("%c", &symbol);
+    printf("Enter number ");
+    scanf("%d", &num);
+    printsymbol(num, symbol);
+
+    printf("\nName : Belim sahil \n");
+    printsymbol(num,symbol);
+
+    printf("\nAge : 18\n");
+
+    printsymbol(num, symbol);
+
+    printf("\nWeight : 45.36\n");
+ 
+    printsymbol(num, symbol);
+  
+    
+
+}
